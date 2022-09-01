a129090971373f66b12781e62278c0da0b43a541
ahmadharori <ahmadharorizaki@gmail.com>
2022-09-01 21:59:10 +0700

    Finish all features

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './views/Home';
+import Detail from './views/Detail';
+import AddPost from './views/AddPost';
+import { BrowserRouter, Switch, Route, Link, Routes } from "react-router-dom";
+import DetailAdd from './views/DetailAdd';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/add' element={< AddPost />}/>
+          <Route path='/detail/:id' element={ <Detail />} />
+          <Route path='/added' element= {< DetailAdd /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
